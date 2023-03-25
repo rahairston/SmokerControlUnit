edb1e0ecda73383081a5b00f70de8beab0468ee1
rahairston <rahairston2014@verizon.net>
2023-03-25 02:07:50 +0000

    Updating codes

--- pi/therm.py
+++ pi/therm.py
@@ -12,29 +12,34 @@ GPIO.setup(so_pin,GPIO.IN)
 
 GPIO.output(cs_pin, 1)
 
+delay_micro = 0.00001
+
+sleep(2)
+
+
 def spiread():
     
-    b = b''
+    b = 0
     
     for i in range(7,-1,-1):
         GPIO.output(sclk_pin, 0)
-        sleep(0.01)
+        sleep(delay_micro)
         if (GPIO.input(so_pin)):
             b |= (1 << i)
         
         GPIO.output(sclk_pin, 1)
-        sleep(0.01)
+        sleep(delay_micro)
         
     return b
 
 
 while(True):
     GPIO.output(cs_pin, 0)
-    sleep(.001)
+    sleep(delay_micro)
     v = spiread()
     v <<= 8
     v |= spiread()
-    GPIO.output(cs_pin, 0)
+    GPIO.output(cs_pin, 1)
 
     if (v & 0x4):
         print("NaN")
@@ -44,7 +49,7 @@ while(True):
     fahrenheit = celsius * 9.0 / 5.0 + 32
     print(celsius, fahrenheit)
 
-    sleep(5)
+    sleep(3)
     
     
 
