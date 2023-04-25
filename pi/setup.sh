ae1514a0cb0d38f12044bcdf09d40de8b408f40f
rahairston <rahairston2014@verizon.net>
2023-04-25 17:54:03 -0500

    Setup finish and ignoring folders

--- pi/setup.sh
+++ pi/setup.sh
@@ -6,7 +6,3 @@ sudo pip3 install -r requirements.txt
 git clone https://github.com/adafruit/Adafruit_Python_MAX31855
 cp -r Adafruit_Python_MAX31855/Adafruit_MAX31855 ./
 rm -rf Adafruit_Python_MAX31855/
-#cp Adafruit_Python_MAX31855/setup.py ./
-#sudo python3 setup.py install
-#rm setup.py
-#rm -rf __pycache__
