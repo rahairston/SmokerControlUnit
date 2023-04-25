89317cce9c0e0ac559706dc8a871360056729054
rahairston <rahairston2014@verizon.net>
2023-04-25 17:50:26 -0500

    Removing extra stuff so its just setup

--- pi/setup.sh
+++ pi/setup.sh
@@ -3,4 +3,10 @@ sudo apt-get install build-essential python-dev python-smbus
 python3 -m venv venv
 . venv/bin/activate
 sudo pip3 install -r requirements.txt
-sudo python3 setup.py install
+git clone https://github.com/adafruit/Adafruit_Python_MAX31855
+cp -r Adafruit_Python_MAX31855/Adafruit_MAX31855 ./
+rm -rf Adafruit_Python_MAX31855/
+#cp Adafruit_Python_MAX31855/setup.py ./
+#sudo python3 setup.py install
+#rm setup.py
+#rm -rf __pycache__
